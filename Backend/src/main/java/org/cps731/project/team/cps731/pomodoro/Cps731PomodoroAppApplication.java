ba2412e79ac2364e9ea6e4765d2467288fd93f5a
John Amiscaray <john_ernest_amiscaray@outlook.com>
2024-11-13 19:19:42 -0500

    Add Spring Boilerplate

---
+++ Backend/src/main/java/org/cps731/project/team/cps731/pomodoro/Cps731PomodoroAppApplication.java
@@ -0,0 +1,13 @@
+package org.cps731.project.team.cps731.pomodoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cps731PomodoroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cps731PomodoroAppApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/org/cps731/project/team/cps731/pomodoro/Cps731PomodoroAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cps731.project.team.cps731.pomodoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cps731PomodoroAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
