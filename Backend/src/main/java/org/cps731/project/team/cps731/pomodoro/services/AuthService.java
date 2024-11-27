15f238bfb56446d0d9a228acd9e223b857aeae7d
John Amiscaray <john_ernest_amiscaray@outlook.com>
2024-11-27 14:36:49 -0500

    Implement student sign up"

--- Backend/src/main/java/org/cps731/project/team/cps731/pomodoro/services/AuthService.java
+++ Backend/src/main/java/org/cps731/project/team/cps731/pomodoro/services/AuthService.java
@@ -1,7 +1,11 @@
 package org.cps731.project.team.cps731.pomodoro.services;
 
 import jakarta.security.auth.message.AuthException;
-import org.cps731.project.team.cps731.pomodoro.dto.LoginRequestDTO;
+import org.cps731.project.team.cps731.pomodoro.data.model.user.Student;
+import org.cps731.project.team.cps731.pomodoro.data.model.user.User;
+import org.cps731.project.team.cps731.pomodoro.data.model.user.UserType;
+import org.cps731.project.team.cps731.pomodoro.data.repo.user.UserRepo;
+import org.cps731.project.team.cps731.pomodoro.dto.AuthRequestDTO;
 import org.cps731.project.team.cps731.pomodoro.security.auth.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,16 +18,35 @@ public class AuthService {
     private final StudentService studentService;
     private final ProfessorService professorService;
     private final PasswordEncoder encoder;
+    private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AuthService(JwtUtil jwtUtil, StudentService studentService, ProfessorService professorService, PasswordEncoder encoder) {
+    public AuthService(JwtUtil jwtUtil,
+                       StudentService studentService,
+                       ProfessorService professorService,
+                       PasswordEncoder encoder,
+                       UserRepo userRepo,
+                       PasswordEncoder passwordEncoder) {
         this.jwtUtil = jwtUtil;
         this.studentService = studentService;
         this.professorService = professorService;
         this.encoder = encoder;
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public String studentLogin(LoginRequestDTO loginRequest) throws AuthException {
+    public boolean studentRegister(AuthRequestDTO registerRequest){
+        if (userRepo.existsByEmail(registerRequest.getEmail())) {
+            throw new IllegalArgumentException("Account using this email already exists");
+        }
+        var user = new User(registerRequest.getEmail(), passwordEncoder.encode(registerRequest.getPassword()), UserType.STUDENT);
+        userRepo.save(user);
+        var student = studentService.createStudent(new Student(user));
+        return student != null;
+    }
+
+    public String studentLogin(AuthRequestDTO loginRequest) throws AuthException {
         var student = studentService.getStudentByEmail(loginRequest.getEmail());
         if (student == null) {
             throw new AuthException("Student not found");
@@ -33,7 +56,7 @@ public class AuthService {
         return jwtUtil.generateToken(student.getUser().getId().toString());
     }
 
-    public String professorLogin(LoginRequestDTO loginRequest) throws AuthException {
+    public String professorLogin(AuthRequestDTO loginRequest) throws AuthException {
         var professor = professorService.getProfessorByEmail(loginRequest.getEmail());
         if (professor == null) {
             throw new AuthException("Professor not found");
