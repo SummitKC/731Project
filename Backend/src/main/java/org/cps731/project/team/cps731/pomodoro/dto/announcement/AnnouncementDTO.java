06cea34dfc06efd279c36f5a4f94436f88099621
John Amiscaray <john_ernest_amiscaray@outlook.com>
2024-11-30 15:11:21 -0500

    Add test for student course page controller. Update DTOs to have equals and hashcode to properly compare them.

--- Backend/src/main/java/org/cps731/project/team/cps731/pomodoro/dto/announcement/AnnouncementDTO.java
+++ Backend/src/main/java/org/cps731/project/team/cps731/pomodoro/dto/announcement/AnnouncementDTO.java
@@ -52,4 +52,50 @@ public class AnnouncementDTO {
     public void setAnnouncementPostDate(String announcementPostDate) {
         this.announcementPostDate = announcementPostDate;
     }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof AnnouncementDTO)) return false;
+        final AnnouncementDTO other = (AnnouncementDTO) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$announcementID = this.getAnnouncementID();
+        final Object other$announcementID = other.getAnnouncementID();
+        if (this$announcementID == null ? other$announcementID != null : !this$announcementID.equals(other$announcementID))
+            return false;
+        final Object this$announcementHeader = this.getAnnouncementHeader();
+        final Object other$announcementHeader = other.getAnnouncementHeader();
+        if (this$announcementHeader == null ? other$announcementHeader != null : !this$announcementHeader.equals(other$announcementHeader))
+            return false;
+        final Object this$announcementPostDate = this.getAnnouncementPostDate();
+        final Object other$announcementPostDate = other.getAnnouncementPostDate();
+        if (this$announcementPostDate == null ? other$announcementPostDate != null : !this$announcementPostDate.equals(other$announcementPostDate))
+            return false;
+        final Object this$announcementDescription = this.getAnnouncementDescription();
+        final Object other$announcementDescription = other.getAnnouncementDescription();
+        if (this$announcementDescription == null ? other$announcementDescription != null : !this$announcementDescription.equals(other$announcementDescription))
+            return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof AnnouncementDTO;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $announcementID = this.getAnnouncementID();
+        result = result * PRIME + ($announcementID == null ? 43 : $announcementID.hashCode());
+        final Object $announcementHeader = this.getAnnouncementHeader();
+        result = result * PRIME + ($announcementHeader == null ? 43 : $announcementHeader.hashCode());
+        final Object $announcementPostDate = this.getAnnouncementPostDate();
+        result = result * PRIME + ($announcementPostDate == null ? 43 : $announcementPostDate.hashCode());
+        final Object $announcementDescription = this.getAnnouncementDescription();
+        result = result * PRIME + ($announcementDescription == null ? 43 : $announcementDescription.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "AnnouncementDTO(announcementID=" + this.getAnnouncementID() + ", announcementHeader=" + this.getAnnouncementHeader() + ", announcementPostDate=" + this.getAnnouncementPostDate() + ", announcementDescription=" + this.getAnnouncementDescription() + ")";
+    }
 }
