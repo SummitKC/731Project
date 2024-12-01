36ae799f13ec2539e99ffcddcb31978d1b885499
Syed Iltefat <syxddd@proton.me>
2024-12-01 03:56:51 -0500

    pomodoro integration with backend + overtime feature

--- frontend/src/components/Pomodoro/Timer.jsx
+++ frontend/src/components/Pomodoro/Timer.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import '../../assets/pomodoro.css';
 import '../../assets/taskboard.css';
 import '../../assets/professorhome.css';
+import { useNavigate } from 'react-router-dom';
 
-const Timer = () => {
+const Timer = ({ tasks }) => {
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
@@ -13,54 +14,70 @@ const Timer = () => {
   const [isPaused, setIsPaused] = useState(true);
   const [isSessionStarted, setIsSessionStarted] = useState(false);
   const [showInputs, setShowInputs] = useState(false);
+  const [isOvertime, setIsOvertime] = useState(false);
+  const [overtimeSeconds, setOvertimeSeconds] = useState(0);
+  const [selectedTaskID, setSelectedTaskID] = useState('');
+  
+  const [selectedTask, setSelectedTask] = useState({
+    "id": 0,
+    "taskName": "",
+    "taskStatus": "",
+    "taskPriority": "",
+    "taskDate": ""
+  });
 
   const [incrementsCompleted, setIncrementsCompleted] = useState(0);
   const [shortBreak, setShortBreak] = useState(false);
   const [longBreak, setLongBreak] = useState(false);
 
+  const navigate = useNavigate();
+  
   const calculateTime = (time) => {
     setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
     setMinutes(Math.floor((time / (1000 * 60)) % 60));
     setSeconds(Math.floor((time / 1000) % 60));
   };
 
+  const playNotificationSound = () => {
+    const audio = new Audio(process.env.PUBLIC_URL + '/notification_sound.wav');
+    audio.play();
+  };
+
+  const token = localStorage.getItem('token');
+
   useEffect(() => {
     let interval = null;
 
-    if (isRunning && !isPaused && timeLeft > 0) {
-      interval = setInterval(() => {
-        setTimeLeft((prevTime) => {
-          if (prevTime <= 1000) {
-            clearInterval(interval);
-            setIsRunning(false);
-            setIsPaused(true);
-
-            // If it's time for a break, handle it
-            if (shortBreak) {
-              setShortBreak(false);
-              startShortBreak();
-            } else if (longBreak) {
-              setLongBreak(false);
-              startLongBreak();
-            } else {
-              handlePomodoroEnd();
-            }
-
-            return 0;
-          }
-          return prevTime - 1000;
-        });
-      }, 1000);
+    if (isRunning && !isPaused) {
+      if (timeLeft > 0) {
+        interval = setInterval(() => {
+          setTimeLeft((prevTime) => prevTime - 1000);
+        }, 1000);
+      } else {
+        if (!isOvertime) {
+          setIsOvertime(true);
+          playNotificationSound();
+        }
+        interval = setInterval(() => {
+          setOvertimeSeconds((prevTime) => prevTime + 1);
+        }, 1000);
+      }
     } else if (!isRunning) {
       clearInterval(interval);
     }
 
     return () => clearInterval(interval);
-  }, [isRunning, isPaused, timeLeft, shortBreak, longBreak]);
+  }, [isRunning, isPaused, timeLeft, isOvertime]);
 
   useEffect(() => {
     calculateTime(timeLeft);
   }, [timeLeft]);
+
+  useEffect(() => {
+    if (isOvertime) {
+      calculateTime(overtimeSeconds * 1000);
+    }
+  }, [overtimeSeconds, isOvertime]);
 
   const handleStartNewSession = () => {
     handleReset();
@@ -68,28 +85,148 @@ const Timer = () => {
     setIsSessionStarted(false);
   };
 
-  const handleStartSession = () => {
+  const getTask = (taskID) => {
+    const token = localStorage.getItem('token');
+  
+    if (!token) {
+      console.log('Expired or bad token, login again!');
+      navigate('/login');
+    } else {
+      fetch(`http://localhost:8080/api/student/taskboard/tasks/${taskID}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `${token}`,
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Task data fetched:', data);
+        setSelectedTask(data);
+      })
+      .catch(error => {
+        console.error('Error fetching task:', error);
+      });
+    }
+  };
+  
+
+  const handleStartSession = async () => {
+    if (!selectedTaskID) {
+      alert("Please select a task before starting the session.");
+      return;
+    }
     const totalMilliseconds = hours * 60 * 60 * 1000 + minutes * 60 * 1000 + seconds * 1000;
     if (totalMilliseconds <= 0) {
       alert("Please set a valid time for the session.");
       return;
     }
-
+  
     setTotalTime(totalMilliseconds);
     setTimeLeft(totalMilliseconds);
+  
+    const mins = totalMilliseconds >= 6000 ? totalMilliseconds / 60000 : totalMilliseconds / 1000;
+    setConvertedTime(mins);
+  
+    const token = localStorage.getItem('token');
+    
+    if (!token) {
+      console.log('Expired or bad token, login again!');
+      navigate('/login');
+    } else {
+      try {
+        const response = await fetch(`http://localhost:8080/api/student/pomodoro/start/${selectedTaskID}?mins=${mins}`, {
+          method: 'POST',
+          headers: {
+            Authorization: `${token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log('Pomodoro session started:', data);
+        await getTask(selectedTaskID);
+        console.log(selectedTask);
+      } catch (error) {
+        console.error('Error starting pomodoro session:', error);
+      }
+    }
+  
     setIsRunning(true);
     setIsPaused(false);
     setIsSessionStarted(true);
     setShowInputs(false);
   };
+  
 
   const handlePauseResume = () => {
     if (isPaused) {
       setIsPaused(false);
       setIsRunning(true);
+      
+      if (!token) {
+        console.log('Expired or bad token, login again!');
+        navigate('/login');
+      } else {
+        fetch(`http://localhost:8080/api/student/pomodoro/resume/${selectedTaskID}`,
+          { 
+            method: 'POST', 
+            headers: 
+            { 
+              Authorization: `${token}`,
+              'Content-Type': 'application/json',
+            },
+            })
+            .then(response => { 
+              if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`); }
+                return response.json(); 
+            })
+            .then(data => { 
+              console.log('session resumed:', data);
+            })   
+            .catch(error => { 
+              console.error('Error resuming pomodoro session:', error);  
+            })
+      }
+      
     } else {
       setIsPaused(true);
       setIsRunning(false);
+      
+      if (!token) {
+        console.log('Expired or bad token, login again!');
+        navigate('/login');
+      } else {
+        fetch(`http://localhost:8080/api/student/pomodoro/pause/${selectedTaskID}`,
+          { 
+            method: 'POST', 
+            headers: 
+            { 
+              Authorization: `${token}`,
+              'Content-Type': 'application/json',
+            },
+            })
+            .then(response => { 
+              if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`); }
+                return response.json(); 
+            })
+            .then(data => { 
+              console.log('session paused:', data);
+            })   
+            .catch(error => { 
+              console.error('Error pausing pomodoro session:', error);  
+            })
+      }
+      
     }
   };
 
@@ -100,6 +237,8 @@ const Timer = () => {
     setShowInputs(false);
     setTotalTime(0);
     setTimeLeft(0);
+    setOvertimeSeconds(0);
+    setIsOvertime(false);
     setHours(0);
     setMinutes(0);
     setSeconds(0);
@@ -121,20 +260,53 @@ const Timer = () => {
   };
 
   const handlePomodoroEnd = () => {
-    if (incrementsCompleted === 3) {
+    /* if (incrementsCompleted === 3) {
       setLongBreak(true);
       setIncrementsCompleted(0); // Reset Pomodoro count after long break
     } else {
       setShortBreak(true);
     }
 
-    setIncrementsCompleted(incrementsCompleted + 1);
+    setIncrementsCompleted(incrementsCompleted + 1); */
+
+    if (!token) {
+      console.log('Expired or bad token, login again!');
+      navigate('/login');
+    } else {
+      fetch(`http://localhost:8080/api/student/pomodoro/end/${selectedTaskID}`,
+        { 
+          method: 'POST', 
+          headers: 
+          { 
+            Authorization: `${token}`,
+            'Content-Type': 'application/json',
+          },
+          })
+          .then(response => { 
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`); }
+              return response.json(); 
+          })
+          .then(data => { 
+            console.log('Pomodoro session ended:', data);
+          })   
+          .catch(error => { 
+            console.error('Error starting pomodoro session:', error);  
+          })
+    }
+    handleReset();
+    setSelectedTask('')
+    
   };
 
   // Calculate circle progress and color
   const totalSessionTime = hours * 3600000 + minutes * 60000 + seconds * 1000;
   const progress = totalTime > 0 ? (1099 * timeLeft) / totalTime : 0;
   const circleColor = timeLeft / totalTime > 0.5 ? 'green' : timeLeft / totalTime > 0.1 ? 'yellow' : 'red';
+
+  const [convertedTime, setConvertedTime] = useState(totalTime);
+  
+  
 
   return (
     <div className="timer">
@@ -152,13 +324,15 @@ const Timer = () => {
             }}
           />
         </svg>
-        <div className="time-display">
+        <div className={`time-display ${isOvertime ? 'overtime' : ''}`}>
           {String(hours).padStart(2, "0")}:{String(minutes).padStart(2, "0")}:{String(seconds).padStart(2, "0")}
           <br />
-          <h1 className="sub-text">Remaining</h1>
+          <h1 className="sub-text">{isOvertime ? 'Overtime' : 'Remaining'}</h1>
         </div>
       </div>
-
+      <h4 style={isSessionStarted ? {} : {display:'none'}} className="font">
+      <strong>{convertedTime} min</strong> session for <strong>{selectedTask.taskName}</strong>
+      </h4>
       <div className="session-buttons">
         <button style={!showInputs ? {} : { display: "none" }} onClick={handleStartNewSession} className="generic-button">
           Start New Session
@@ -167,10 +341,6 @@ const Timer = () => {
         {showInputs && (
           <div className="input-section">
             <div>
-              <button onClick={handleStartSession} className="generic-button">
-                Start Session
-              </button>
-              <br />
               <label className="user-input">
                 Hours:
                 <input
@@ -181,8 +351,6 @@ const Timer = () => {
                   onChange={(e) => setHours(Number(e.target.value))}
                 />
               </label>
-            </div>
-            <div>
               <label className="user-input">
                 Minutes:
                 <input
@@ -194,16 +362,32 @@ const Timer = () => {
                   onChange={(e) => setMinutes(Number(e.target.value))}
                 />
               </label>
+
+              <label>Select a Task to work on:</label>
+              <br />
+              <select value={selectedTaskID} onChange={(e) => setSelectedTaskID(e.target.value)} className="task-select">
+                <option value="" disabled>Select a Task</option>
+                {tasks.map(task => (
+                  <option key={task.id} value={task.id}>{task.taskName}</option>
+                ))}
+              </select>
+              <br />
+              <button onClick={handleStartSession} className="generic-button">
+                Start Session
+              </button>
             </div>
           </div>
         )}
 
-        <button onClick={handlePauseResume} className="generic-button">
+        <button style={isSessionStarted ? {} : {display:'none'}} onClick={handlePauseResume} className="generic-button">
           {isRunning && !isPaused ? "Pause Session" : "Resume Session"}
         </button>
 
-        <button onClick={handleReset} className="generic-button">
-          End Session
+        <button style={isSessionStarted ? {} : {display:'none'}} onClick={handlePomodoroEnd} className="generic-button">
+          End Session (Recorded)
+        </button>
+        <button style={isSessionStarted ? {} : {display:'none'}} onClick={handleReset} className="generic-button">
+          Delete Session
         </button>
       </div>
     </div>
--- frontend/src/pages/PomodoroPage.js
+++ frontend/src/pages/PomodoroPage.js
@@ -4,41 +4,63 @@ import '../assets/global.css';
 import { useMediaQuery } from 'react-responsive';
 import StudentSidebar from '../components/Common/StudentSidebar';
 import Timer from '../components/Pomodoro/Timer';
-import Task from '../components/TaskBoard/Task';
 import '../assets/task.css';
 import '../assets/pomodoro.css';
+import { useNavigate } from 'react-router-dom';
 
 const PomodoroPage = () => {
-    const isMobile = useMediaQuery({ query: '(max-width: 700px)' });
-    const isTablet = useMediaQuery({ query: '(max-width: 1224px)' });
-    const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 1224px)' });
-    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
-  
-    const firstName = localStorage.getItem('name')?.split(' ')[0] || " ";
-    const lastName = localStorage.getItem('name')?.split(' ')[1] || " ";
-    const initials = `${firstName[0]}${lastName[0]}`;
+  const isMobile = useMediaQuery({ query: '(max-width: 700px)' });
+  const firstName = localStorage.getItem('name')?.split(' ')[0] || " ";
+  const lastName = localStorage.getItem('name')?.split(' ')[1] || " ";
+  const initials = `${firstName[0]}${lastName[0]}`;
 
+  const [tasks, setTasks] = useState([]);
+  const navigate = useNavigate();
 
-  
-    return (
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <StudentSidebar firstName={firstName} lastName={lastName} />
-        <div style={{ width: '100vw' }}>
-          <div id="profile-container" style={isMobile ? {} : { display: 'none' }}>
-            <div className="profile-placeholder">{initials}</div>
-            <div className="name">{firstName} {lastName}</div>
-          </div>
-          
-          <div className="main-content main-container">
-              <h1 className='header-container'>Pomodoro</h1>     
+  useEffect(() => {
+    const token = localStorage.getItem('token');
     
-            <Timer className="timer"/>
-            </div>
-            
-          </div>
+    if (!token) {
+      console.log('Expired or bad token, login again!');
+      navigate('/login');
+    } else {
+      // get all tasks
+      fetch('http://localhost:8080/api/student/taskboard/tasks', {
+        method: 'GET',
+        headers: {
+          Authorization: `${token}`,
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        const fetchedTasks = [
+          ...data.todoTasks,
+          ...data.inProgressTasks,
+          ...data.reviewingTasks
+        ];
+        setTasks(fetchedTasks);
+      })
+      .catch(error => console.error('Error fetching tasks:', error));
+    }
+  }, [navigate]);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <StudentSidebar firstName={firstName} lastName={lastName} />
+      <div style={{ width: '100vw' }}>
+        <div id="profile-container" style={isMobile ? {} : { display: 'none' }}>
+          <div className="profile-placeholder">{initials}</div>
+          <div className="name">{firstName} {lastName}</div>
         </div>
-    );
-  }
-  
-  export default PomodoroPage;
-  
+        
+        <div className="main-content main-container">
+          <h1 className='header-container'>Pomodoro</h1>
+          <Timer tasks={tasks} className="timer" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PomodoroPage;
